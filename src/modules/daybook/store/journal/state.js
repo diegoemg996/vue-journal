4a395f245b2318c4e01704e03f7d902a1d4aa10a
Diego <diegoemg996@gmail.com>
2021-11-15 14:45:46 -0600

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: "Do deserunt quis sint cillum dolore. Laborum esse id ad ad labore fugiat anim consequat exercitation labore. Amet enim esse quis enim dolor aute et laboris duis incididunt in. Laborum quis et labore est consequat eiusmod excepteur sint. Dolor velit dolor non exercitation fugiat nostrud aliqua tempor ea culpa. Lorem amet sunt adipisicing fugiat ut.",
+            picture: null
+        },
+        {
+            id: new Date().getTime()+1000,
+            date: new Date().toDateString(),
+            text: "Dolor et ad ad consectetur magna pariatur qui velit qui ea mollit fugiat magna. Sint magna incididunt ad quis consequat consectetur eu reprehenderit in et esse labore. Fugiat amet officia eiusmod anim aliqua magna id ad.",
+            picture: null 
+        },
+        {
+            id: new Date().getTime()+2000,
+            date: new Date().toDateString(),
+            text: "Culpa cupidatat dolor ullamco consequat id id cillum. Id culpa elit quis mollit aliquip Lorem consequat excepteur eu sunt voluptate irure. Sint do nisi ipsum adipisicing cillum mollit excepteur dolor. Consectetur aliquip non commodo laboris quis veniam incididunt sunt eiusmod est. Incididunt incididunt elit ullamco sint culpa officia voluptate et esse commodo laboris. Ad id sint adipisicing Lorem fugiat ad occaecat sint Lorem ea mollit cillum voluptate.",
+            picture: null 
+        }
+
+        
+    ]
+})
